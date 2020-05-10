eca81d79b020bd924e276352f57e0fb726b439d4
berat <beratkirik@hotmail.com>
2020-05-10 08:03:20 -0700

    simpleLog eklendi. Tag laer init edildiği yerden okunuyor. 7 adet element var. Gönderilen mesajlardaki tag sayısı artarsa buradan bakılacak. sıralama struct içerisindeki sırasına göredir. 0-1-2..6 gibi.

---
+++ cmake-build-debug/CMakeFiles/nanolog.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/nanolog.dir/NanoLog.cpp.o"
+  "libnanolog.pdb"
+  "libnanolog.so"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/nanolog.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/simplog.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/simplog.dir/simplog.c.o"
+  "libsimplog.pdb"
+  "libsimplog.so"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/simplog.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
